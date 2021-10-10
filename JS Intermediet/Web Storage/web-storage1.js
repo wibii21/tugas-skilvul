d08de8c83bea42d0e9578536e7ca828de7ff8099
Hermawan Wibisono <90597031+wibii21@users.noreply.github.com>
2021-10-10 23:28:00 +0700

    Add files via upload

---
+++ JS Intermediet/Web Storage/web-storage1.js
@@ -0,0 +1,6 @@
+///Apa itu localStorage?
+//Local storage merupakan tempat di browser yang bisa digunakan untuk menyimpan data dan data tidak akan hilang walaupun browser di close dan komputer di restart. Local storage sendiri bisa digunakan untuk menyimpan konfigurasi yang diperlukan.
+//Apa itu cookies?
+//Cookie adalah istilah untuk kumpulan informasi yang berisi rekam jejak dan aktivitas ketika menelusuri sebuah website. Secara sederhana pengertian cookies adalah kumpulan data yang diterima komputer dari sebuah situs dan mengirimkan kembali ke situs yang dikunjungi.
+//Apa perbedaan antara localStorage dan sessionStorage?
+//localStorage menyimpan informasi selama pengguna tidak menghapusnya. sessionStorage menyimpan informasi selama sesi berlangsung. Biasanya sampai pengguna menutup tab browser.
